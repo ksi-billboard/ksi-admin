2e032d9f1c62cf72806db38657f3bcc2fb0b7806
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-09 19:26:07 +0700

    Add billboard table template and get.js file

---
+++ js/billboard/get.js
@@ -0,0 +1,26 @@
+import { addInner } from "https://jscroot.github.io/element/croot.js";
+import { getWithToken } from "../temp/component.js";
+import { tableBill } from "../temp/table.js";
+
+const target_url = "https://asia-southeast2-keamanansistem.cloudfunctions.net/billboard"
+
+const dataBill  = (value) => {
+    console.log("value: ", value);
+
+    const data = tableBill
+    .replace("#GAMBAR#", value.file)
+    .replace("#NAMA#", value.nama)
+    .replace("#PANJANG#", value.panjang)
+    .replace("#LEBAR#", value.lebar)
+    .replace("#HARGA#", value.harga)
+
+    addInner("tableAllBill", data);
+}
+
+const responseData = (result) => {
+    if (result.status === true) {
+        result.data.forEach(dataBill);
+    }
+}
+
+getWithToken(target_url, responseData);
---
+++ js/temp/table.js
@@ -0,0 +1,18 @@
+export const tableBill = `
+<li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+<a href="single-billboard.html"></a>
+<div class="job-listing-logo">
+  <img src="#GAMBAR#" alt="Free Website Template by Free-Template.co" class="img-fluid" />
+</div>
+
+<div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
+  <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
+    <h2>#NAMA#</h2>
+    <strong>#PANJANG# x #LEBAR#</strong>
+  </div>
+  <div class="job-listing-meta">
+    <span class="badge badge-danger">Rp. #HARGA#</span>
+  </div>
+</div>
+</li>
+`;
