57883d1b904d39598e91189a3c1ef257a539bfc4
dirgafebrian <fdirga63@gmail.com>
2024-01-10 12:06:04 +0700

    Refactor isiData function and add default values for getNestedValue

--- js/billboard/edit.js
+++ js/billboard/edit.js
@@ -1,42 +1,40 @@
 export const isiData = (results) => {
-  console.log("isiData:", results);
-  const dataBil = [
-    {id: "gambar", path: "data.gambar"},
-    {id: "kode", path: "data.kode"},
-    {id: "nama", path: "data.nama"},
-    {id: "panjang", path: "data.panjang"},
-    {id: "lebar", path: "data.lebar"},
-    {id: "harga", path: "data.harga"},
-    {id: "regency", path: "data.regency"},
-    {id: "district", path: "data.district"},
-    {id: "village", path: "data.village"},
-    {id: "address", path: "data.address"},
-    {id: "latitude", path: "data.latitude"},
-    {id: "longitude", path: "data.longitude"},
-  ];
-
-
-  dataBil.forEach(({ id, path, index, property }) => {
-    const inputElement = document.getElementById(id);
-    const value = getNestedValue(results, path, index, property);
-    inputElement.value = value;
-  });
-}
-
-const getNestedValue = (obj, path, index, property) => {
-  const value = path
-    .split(".")
-    .reduce((value, key) => (value && value[key] !== undefined ? value[key] : ""), obj);
-
-  if (
-    Array.isArray(value) &&
-    value.length > index &&
-    value[index].hasOwnProperty(property)
-  ) {
-    return value[index][property];
-  }
-
-  return value;
-};
-
+    console.log("isiData:", results);
+    const dataBil = [
+      { id: "gambar", path: "data.gambar" },
+      { id: "kode", path: "data.kode" },
+      { id: "nama", path: "data.nama" },
+      { id: "panjang", path: "data.panjang" },
+      { id: "lebar", path: "data.lebar" },
+      { id: "harga", path: "data.harga" },
+      { id: "regency", path: "data.regency" },
+      { id: "district", path: "data.district" },
+      { id: "village", path: "data.village" },
+      { id: "address", path: "data.address" },
+      { id: "latitude", path: "data.latitude" },
+      { id: "longitude", path: "data.longitude" },
+    ];
+  
+    dataBil.forEach(({ id, path, index, property }) => {
+      const inputElement = document.getElementById(id);
+      const value = getNestedValue(results, path, index, property);
+      inputElement.value = value;
+    });
+  };
+  
+  const getNestedValue = (obj, path, index = 0, property = "") => {
+    const value = path
+      .split(".")
+      .reduce((value, key) => (value && value[key] !== undefined ? value[key] : ""), obj);
+  
+    if (
+      Array.isArray(value) &&
+      value.length > index &&
+      value[index].hasOwnProperty(property)
+    ) {
+      return value[index][property];
+    }
+  
+    return value;
+  };
   
