b8bbe25c1bb5a5a02c9af71d8131a545991cd75e
dirgafebrian <fdirga63@gmail.com>
2024-01-10 12:37:48 +0700

    Refactor isiData function and update inputMapping

--- js/billboard/edit.js
+++ js/billboard/edit.js
@@ -1,44 +1,38 @@
 export const isiData = (results) => {
-    console.log("isiData:", results);
-    const inputMapping = [
-      { id: "gambar", path: "gambar" },
-      { id: "kode", path: "kode" },
-      { id: "nama", path: "nama" },
-      { id: "panjang", path: "panjang" },
-      { id: "lebar", path: "lebar" },
-      { id: "harga", path: "harga" },
-      { id: "regency", path: "regency" },
-      { id: "district", path: "district" },
-      { id: "village", path: "village" },
-      { id: "address", path: "address" },
-      { id: "latitude", path: "latitude" },
-      { id: "longitude", path: "longitude" },
-    ];
-  
-    inputMapping.forEach(({ id, path, index, property }) => {
-      const inputElement = document.getElementById(id);
-      const value = getNestedValue(results, path, index, property);
-      console.log("value:", value);
-      if (inputElement.tagName === "IMG") {
-        inputElement.src = value ? value : defaultImageUrl;
-      } else {
-        inputElement.value = value;
-      }
-    });
-  };
-  
-  const getNestedValue = (obj, path, index, property) => {
-    const value = path
-      .split(".")
-      .reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
-  
-    if (
-      Array.isArray(value) &&
-      value.length > index &&
-      value[index].hasOwnProperty(property)
-    ) {
-      return value[index][property];
-    }
-  
-    return value;
-  };
+  console.log("isiData:", results);
+
+  const inputMapping = [
+    { id: "kode", path: "data", property: "kode" },
+    { id: "nama", path: "data", property: "nama" },
+    { id: "panjang", path: "data", property: "panjang" },
+    { id: "lebar", path: "data", property: "lebar" },
+    { id: "harga", path: "data", property: "harga" },
+    { id: "regency", path: "data", property: "regency" },
+    { id: "district", path: "data", property: "district" },
+    { id: "village", path: "data", property: "village" },
+    { id: "address", path: "data", property: "address" },
+    { id: "latitude", path: "data", property: "latitude" },
+    { id: "longitude", path: "data", property: "longitude" },
+  ];
+
+  inputMapping.forEach((input) => {
+    const value = getNestedValue(results, input.path, 0, input.property);
+    console.log("value:", value);
+    document.getElementById(input.id).value = value;
+  });
+}
+
+const getNestedValue = (obj, path, defaultValue, property) => {
+  const travel = (regexp) =>
+    String.prototype.split
+      .call(path, regexp)
+      .filter(Boolean)
+      .reduce(
+        (res, key) => (res !== null && res !== undefined ? res[key] : res),
+        obj
+      );
+
+  const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
+  return result === undefined || result === obj ? defaultValue : result[property];
+};
+
