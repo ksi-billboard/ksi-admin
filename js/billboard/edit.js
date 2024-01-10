a90e7180cda6b0d718561a67f5648f16a1519de4
dirgafebrian <fdirga63@gmail.com>
2024-01-10 12:28:06 +0700

    Refactor inputMapping in isiData function

--- js/billboard/edit.js
+++ js/billboard/edit.js
@@ -1,25 +1,29 @@
 export const isiData = (results) => {
     console.log("isiData:", results);
     const inputMapping = [
-      { id: "gambar", path: "data.gambar" },
-      { id: "kode", path: "data.kode" },
-      { id: "nama", path: "data.nama" },
-      { id: "panjang", path: "data.panjang" },
-      { id: "lebar", path: "data.lebar" },
-      { id: "harga", path: "data.harga" },
-      { id: "regency", path: "data.regency" },
-      { id: "district", path: "data.district" },
-      { id: "village", path: "data.village" },
-      { id: "address", path: "data.address" },
-      { id: "latitude", path: "data.latitude" },
-      { id: "longitude", path: "data.longitude" },
+      { id: "gambar", path: "gambar" },
+      { id: "kode", path: "kode" },
+      { id: "nama", path: "nama" },
+      { id: "panjang", path: "panjang" },
+      { id: "lebar", path: "lebar" },
+      { id: "harga", path: "harga" },
+      { id: "regency", path: "regency" },
+      { id: "district", path: "district" },
+      { id: "village", path: "village" },
+      { id: "address", path: "address" },
+      { id: "latitude", path: "latitude" },
+      { id: "longitude", path: "longitude" },
     ];
   
     inputMapping.forEach(({ id, path, index, property }) => {
       const inputElement = document.getElementById(id);
       const value = getNestedValue(results, path, index, property);
       console.log("value:", value);
-      inputElement.value = value;
+      if (inputElement.tagName === "IMG") {
+        inputElement.src = value ? value : defaultImageUrl;
+      } else {
+        inputElement.value = value;
+      }
     });
   };
   
