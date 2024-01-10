113351e7bf90d9b8deb30a22a9e144f9419a708b
dirgafebrian <fdirga63@gmail.com>
2024-01-10 13:14:44 +0700

    Commented out code related to extracting file name from URL

--- js/billboard/edit.js
+++ js/billboard/edit.js
@@ -1,7 +1,7 @@
 export const isiData = (results) => {
   console.log("isiData:", results);
   const inputMapping = [
-    { id: "gambar", path: "data.gambar" },
+    // { id: "gambar", path: "data.gambar" },
     { id: "kode", path: "data.kode" },
     { id: "nama", path: "data.nama" },
     { id: "panjang", path: "data.panjang" },
@@ -20,10 +20,10 @@ export const isiData = (results) => {
     const value = getNestedValue(results, path, index, property);
     console.log("value:", value);
 
-    const fileName = extractFileNameFromUrl(value);
-    const fileNameElement = document.getElementById("gambar");
-    fileNameElement.textContent = fileName;
-    console.log("fileName:", fileName);
+    // const fileName = extractFileNameFromUrl(value);
+    // const fileNameElement = document.getElementById("gambar");
+    // fileNameElement.textContent = fileName;
+    // console.log("fileName:", fileName);
 
     inputElement.value = value;
     
@@ -47,9 +47,9 @@ const getNestedValue = (obj, path, index, property) => {
 };
 
 // Fungsi untuk mengekstrak nama file dari URL
-function extractFileNameFromUrl(url) {
-  const pathArray = url.split('/');
-  const fileName = pathArray[pathArray.length - 1];
-  const fileNameWithoutExtension = fileName.split('.')[0];
-  return fileNameWithoutExtension;
-}
+// function extractFileNameFromUrl(url) {
+//   const pathArray = url.split('/');
+//   const fileName = pathArray[pathArray.length - 1];
+//   const fileNameWithoutExtension = fileName.split('.')[0];
+//   return fileNameWithoutExtension;
+// }
