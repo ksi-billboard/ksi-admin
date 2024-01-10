59ea3f1bf4889460d82febadf532808d75df4ebd
dirgafebrian <fdirga63@gmail.com>
2024-01-10 13:09:02 +0700

    Add function to extract file name from URL and display it

--- js/billboard/edit.js
+++ js/billboard/edit.js
@@ -1,45 +1,60 @@
 export const isiData = (results) => {
-    console.log("isiData:", results);
-    const inputMapping = [
-      { id: "gambar", path: "data.gambar" },
-      { id: "kode", path: "data.kode" },
-      { id: "nama", path: "data.nama" },
-      { id: "panjang", path: "data.panjang" },
-      { id: "lebar", path: "data.lebar" },
-      { id: "harga", path: "data.harga" },
-      { id: "regency", path: "data.regency" },
-      { id: "district", path: "data.district" },
-      { id: "village", path: "data.village" },
-      { id: "address", path: "data.address" },
-      { id: "latitude", path: "data.latitude" },
-      { id: "longitude", path: "data.longitude" },
-    ];
-  
-    inputMapping.forEach(({ id, path, index, property }) => {
-      const inputElement = document.getElementById(id);
-      const value = getNestedValue(results, path, index, property);
-      console.log("value:", value);
-      inputElement.value = value;
+  console.log("isiData:", results);
+  const inputMapping = [
+    { id: "gambar", path: "data.gambar" },
+    { id: "kode", path: "data.kode" },
+    { id: "nama", path: "data.nama" },
+    { id: "panjang", path: "data.panjang" },
+    { id: "lebar", path: "data.lebar" },
+    { id: "harga", path: "data.harga" },
+    { id: "regency", path: "data.regency" },
+    { id: "district", path: "data.district" },
+    { id: "village", path: "data.village" },
+    { id: "address", path: "data.address" },
+    { id: "latitude", path: "data.latitude" },
+    { id: "longitude", path: "data.longitude" },
+  ];
 
-      if (id === "gambar") {
-        const imageElement = document.getElementById("gambar");
-        imageElement.src = value;
-      }
-    });
-  };
-  
-  const getNestedValue = (obj, path, index, property) => {
-    const value = path
-      .split(".")
-      .reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
-  
-    if (
-      Array.isArray(value) &&
-      value.length > index &&
-      value[index].hasOwnProperty(property)
-    ) {
-      return value[index][property];
+  inputMapping.forEach(({ id, path, index, property }) => {
+    const inputElement = document.getElementById(id);
+    const value = getNestedValue(results, path, index, property);
+    console.log("value:", value);
+    inputElement.value = value;
+
+    if (id === "gambar") {
+      const imageElement = document.getElementById("gambar");
+      imageElement.src = value;
+
+      // Mengekstrak nama file dari URL gambar
+      const fileName = extractFileNameFromUrl(value);
+
+      // Menampilkan nama file di elemen dengan ID "gambarFileName"
+      const fileNameElement = document.getElementById("gambarFileName");
+      fileNameElement.textContent = fileName;
     }
-  
-    return value;
-  };
+  });
+};
+
+const getNestedValue = (obj, path, index, property) => {
+  const value = path
+    .split(".")
+    .reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
+
+  if (
+    Array.isArray(value) &&
+    value.length > index &&
+    value[index].hasOwnProperty(property)
+  ) {
+    return value[index][property];
+  }
+
+  return value;
+};
+
+// Fungsi untuk mengekstrak nama file dari URL
+function extractFileNameFromUrl(url) {
+  const pathArray = url.split('/');
+  const fileName = pathArray[pathArray.length - 1];
+  const fileNameWithoutExtension = fileName.split('.')[0];
+  return fileNameWithoutExtension;
+}
