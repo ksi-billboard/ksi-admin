86616f417aa77b2fa65861fefd8a9c1fc2472159
dirgafebrian <fdirga63@gmail.com>
2024-01-11 10:32:27 +0700

    Add "List Sewa Billboard" link to navigation menu

--- js/temp/table.js
+++ js/temp/table.js
@@ -46,3 +46,26 @@ export const singleTableBill2 = `
 <td> #LATITUDE# </td>
 <td> #LONGITUDE# </td>
 `;
+
+export const tableSewa = `
+<ul class="job-listings mb-3">
+  <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+    <a href="single-sewa.html?id=#ID#"></a>
+    <div class="job-listing">
+      <img src="#GAMBAR#" class="img-fluid rounded-0" style="width: 300px; height: 150px;"/>
+    </div>
+
+    <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
+      <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
+      <h2><b>Tanggal Mulai : </b> #MULAI#</h2>
+      <h2><b>Tanggal Selesai : </b> #SELESAI#</h2> 
+      </div>
+      <div class="job-listing-meta mx-4">
+          <div class="job-listing-position">
+          <a href="#" class="btn btn-primary flex-grow-1 ml-6"></a>
+          </div>
+      </div>
+    </div>
+  </li>
+</ul>
+`;
