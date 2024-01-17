e251395c49f949039a142bd9d446800d280cf7c2
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-17 18:59:46 +0700

    Add singleTableBill, singleTableBill1, and singleTableBill2 to getID.js

--- js/sewa/getID.js
+++ js/sewa/getID.js
@@ -1,11 +1,47 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { getWithToken } from "../temp/component.js";
-import { singleTableSewa, singleTableSewa1, singleTableSewa2 } from "../temp/table.js";
+import { singleTableBill, singleTableBill1, singleTableBill2, singleTableSewa, singleTableSewa1, singleTableSewa2 } from "../temp/table.js";
 
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 const target_url = "https://asia-southeast2-keamanansistem.cloudfunctions.net/sewa?id=" + id;
+
+const dataBill  = (value) => {
+    const data = singleTableBill
+    .replace("#GAMBAR#", value.gambar)
+
+    addInner("bill", data);
+
+    console.log(data);
+}
+
+const dataBill1  = (value) => {
+    const data = singleTableBill1
+    .replace("#KODE#", value.kode)
+    .replace("#NAMA#", value.nama)
+    .replace("#PANJANG#", value.panjang)
+    .replace("#LEBAR#", value.lebar)
+    .replace("#HARGA#", value.harga)
+
+    addInner("bill1", data);
+
+    console.log(data);
+}
+
+const dataBill2  = (value) => {
+    const data = singleTableBill2
+    .replace("#REGENCY#", value.regency)
+    .replace("#DISTRICT#", value.district)
+    .replace("#VILLAGE#", value.village)
+    .replace("#ADDRESS#", value.address)
+    .replace("#LATITUDE#", value.latitude)
+    .replace("#LONGITUDE#", value.longitude)
+
+    addInner("bill2", data);
+
+    console.log(data);
+}
 
 const dataSewa  = (value) => {
     const data = singleTableSewa
@@ -34,6 +70,9 @@ const dataSewa2  = (value) => {
 
 const responseData = (result) => {
     if (result.status === 200) {
+        dataBill(result.data);
+        dataBill1(result.data);
+        dataBill2(result.data);
         dataSewa(result.data);
         dataSewa1(result.data);
         dataSewa2(result.data);
