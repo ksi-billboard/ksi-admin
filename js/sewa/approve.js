cd9937a23fa07d9354e157a4c8523da75a9530c5
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-25 01:15:53 +0700

    Refactor approveSewa function and add putData helper function

--- js/sewa/approve.js
+++ js/sewa/approve.js
@@ -1,46 +1,48 @@
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-const approveSewa = async (APPROVE) => {
-  const id = APPROVE;
-  const token = getCookie("Authorization");
+const approveSewa = (APPROVE) => {
+    const id = APPROVE;
 
-  const isConfirmed = await Swal.fire({
-    text: "Benarkah anda ingin approve data ini?",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Benar",
-    cancelButtonText: "Tidak",
-  });
-
-  if (isConfirmed.isConfirmed) {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", token);
+    console.log("ID Parameter:", id);
 
     const target_url = "https://asia-southeast2-keamanansistem.cloudfunctions.net/sewa?id=" + id;
-
-    try {
-      const response = await fetch(target_url, {
-        method: "PUT",
-        headers: myHeaders,
-        redirect: "follow",
-      });
-
-      if (response.ok) {
-        await Swal.fire({
-          icon: "success",
-          text: "Data berhasil diapprove",
-          showConfirmButton: false,
-        });
-        location.reload();
-      } else {
-        throw new Error("Request failed with status: " + response.status);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
+    
+    putData(target_url, responseData);
 };
+
+const responseData = (result) => {
+    console.log("Server Response:", result, result.status);
+    if (result.status === 200) {
+        Swal.fire({
+            icon: "success",
+            title: "Approve Successful",
+            text: result.message,
+        }).then(() => {
+            window.location.href = "list-sewa.html";
+        });
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Approve Failed",
+            text: result.message,
+        });
+    }
+}
+
+function putData(target_url, responseFunction) {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", getCookie("Authorization"));
+
+  const requestOptions = {
+      method: 'PUT',
+      headers: myHeaders,
+      redirect: 'follow'
+  };
+
+  fetch(target_url, requestOptions)
+      .then(response => response.text())
+      .then(result => responseFunction(JSON.parse(result)))
+      .catch(error => console.log('error', error));
+}
 
 window.approveSewa = approveSewa;
