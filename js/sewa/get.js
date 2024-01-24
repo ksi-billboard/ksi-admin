8085fa8e8282b4432e6ad352890e8c047e47297c
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-25 01:27:34 +0700

    Add tableSewaApprove to dataSewa function

--- js/sewa/get.js
+++ js/sewa/get.js
@@ -1,20 +1,30 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { getWithToken } from "../temp/component.js";
-import { tableSewa } from "../temp/table.js";
+import { tableSewa, tableSewaApprove } from "../temp/table.js";
 
 const target_url = "https://asia-southeast2-keamanansistem.cloudfunctions.net/sewa"
 
 const dataSewa  = (value) => {
-    const data = tableSewa
-    .replace("#GAMBAR#", value.content)
-    .replace("#MULAI#", value.tanggal_mulai)
-    .replace("#SELESAI#", value.tanggal_selesai)
-    .replace("#ID#", value._id)
-    .replace("#APPROVE#", value._id)
+    if (value.status) {
+        const data = tableSewaApprove
+        .replace("#GAMBAR#", value.content)
+        .replace("#MULAI#", value.tanggal_mulai)
+        .replace("#SELESAI#", value.tanggal_selesai)
+        .replace("#ID#", value._id)
+        // .replace("#APPROVE#", value._id)
 
-    addInner("tableAllSewa", data);
+        addInner("tableAllSewa", data);
 
-    console.log(data);
+    } else {
+        const data = tableSewa
+        .replace("#GAMBAR#", value.content)
+        .replace("#MULAI#", value.tanggal_mulai)
+        .replace("#SELESAI#", value.tanggal_selesai)
+        .replace("#ID#", value._id)
+        .replace("#APPROVE#", value._id)
+
+        addInner("tableAllSewa", data);
+    }
 }
 
 const responseData = (result) => {
--- js/temp/table.js
+++ js/temp/table.js
@@ -68,7 +68,27 @@ export const tableSewa = `
     </div>
   </li>
 </ul>
+`;
 
+export const tableSewaApprove = `
+<ul class="job-listings mb-3" style="background-color: #FFC0CB;">
+  <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+    <a href="single-sewa.html?id=#ID#"></a>
+    <div class="job-listing">
+      <img src="#GAMBAR#" class="img-fluid rounded-0" style="width: 300px; height: 150px;"/>
+    </div>
+
+    <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
+      <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
+        <h2><b>Tanggal Mulai : </b> #MULAI#</h2>
+        <h2><b>Tanggal Selesai : </b> #SELESAI#</h2> 
+      </div>
+      <div class="job-listing-meta">
+        <span class="badge badge-danger approved" style="font-size: 20px; font-style: italic;">Approved</span>
+      </div>
+    </div>
+  </li>
+</ul>
 `;
 
 export const singleTableSewa = `
